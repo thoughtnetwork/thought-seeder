c039b4a422dc8aec7c486bd5b20b4145d5de82e6
Andrew J Hacker <andrew@thought.live>
2020-02-16 12:08:19 -0500

    update mainnet block height and v18

--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 70017
+#define REQUIRE_VERSION 70018
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 200000 : 500000;
+    return testnet ? 200000 : 617000;
 }
 
 std::string static inline ToString(const CService &ip) {
